9ce98674e889c04816687a4e824455835f88bdfa
Paul V <paul.velet@gmail.com>
2025-07-09 18:34:00 -0500

    updates to contactus form

--- js/contactus.js
+++ js/contactus.js
@@ -17,7 +17,7 @@
   const form = document.getElementById('contactForm');
   const modalEl = document.getElementById('thankYouModal');
   const modal = new bootstrap.Modal(modalEl);
-  const actionURL = "https://formsubmit.co/ajax/director@wavevolleyball.net";
+  const actionURL = "https://formsubmit.co/ajax/ad97350ae8aabea0ce5800ec9f17ca7f";
 
   form.addEventListener('submit', async function (e) {
     e.preventDefault();
